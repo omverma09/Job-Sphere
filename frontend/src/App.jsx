d70d9a87d7180a6360e08142d3015be803c357fe
om verma <omver9644@gmail.com>
2026-01-21 14:05:51 -0500

    fix all impo of app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PublicLayout from "./layouts/PublicLayout.jsx"
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AllPublicPages from "./layouts/AllPublicPages";
-import DashboardLayout from "./layouts/dashboardLayout";
-import DashboardAllPages from "./components/dashboardcompo/DashboardAllPages";
-import { AuthProvider } from "./context/AuthContext";
-import JobsPage from "./pages/JobsPage";
-import NotFound from "./pages/NotFound";
-import JobDetails from "./components/dashboardcompo/JobDetails ";
-import Prepration from "./components/dashboardcompo/Prepration";
-import Courses from "./components/dashboardcompo/Courses";
-import RecruiterLayout from "./layouts/RecruiterLayout";
-import PostJob from "./components/recruiterDashboard/PostJob";
-import MyJobs from "./components/recruiterDashboard/MyJobs";
-import UpdateProfileForm from "./components/dashboardcompo/UpdateProfileForm";
-import Applicants from "./components/recruiterDashboard/Applicants";
-import MyApplications from "./components/dashboardcompo/MyApplications";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import AllPublicPages from "./layouts/AllPublicPages.jsx";
+import DashboardLayout from "./layouts/DashboardLayout.jsx"
+import DashboardAllPages from "./components/dashboardcompo/DashboardAllPages.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import JobsPage from "./pages/JobsPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import JobDetails from "./components/dashboardcompo/JobDetails.jsx";
+import Prepration from "./components/dashboardcompo/Prepration.jsx";
+import Courses from "./components/dashboardcompo/Courses.jsx";
+import RecruiterLayout from "./layouts/RecruiterLayout.jsx";
+import PostJob from "./components/recruiterDashboard/PostJob.jsx";
+import MyJobs from "./components/recruiterDashboard/MyJobs.jsx";
+import UpdateProfileForm from "./components/dashboardcompo/UpdateProfileForm.jsx";
+import Applicants from "./components/recruiterDashboard/Applicants.jsx";
+import MyApplications from "./components/dashboardcompo/MyApplications.jsx";
 import ProtectRoute from "./routes/ProtectRoute.jsx"
 
 function App() {
